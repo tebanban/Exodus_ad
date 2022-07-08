5f914348faa2f6b114b58de076b282a6e746b6a7
tebanban <estebancalf@gmail.com>
2022-07-08 00:19:05 -0500

    Add script to move background

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const setTheme= theme=>
 document.documentElement.className = theme;
+
+// let antelope =document.getElementById('antelope_img');
+// window.addEventListener('scroll', function(){
+// var value = window.scrollY;
+// antelope_img.style.top = value * 0.5 + 'px';
+// })
