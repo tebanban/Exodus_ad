eead22e6e168dcd5d60ee5db2e0ff5fbf3da0362
tebanban <estebancalf@gmail.com>
2022-07-09 16:09:09 -0500

    Add script include style.top

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const setTheme= theme=>
 document.documentElement.className = theme;
 
-// let antelope =document.getElementById('antelope_img');
-// window.addEventListener('scroll', function(){
-// var value = window.scrollY;
-// antelope_img.style.top = value * 0.5 + 'px';
-// })
+let ante = document.getElementById("antelope");
+window.addEventListener('scroll', function(){
+var value = window.scrollY;
+antelope.style.top =  value * 0.01 + 'px';
+console.log(antelope.style.top)
+})
