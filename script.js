44bff14df20a2109dcf8b0d23e7af572c9b48897
tebanban <estebancalf@gmail.com>
2022-07-10 13:27:08 -0500

    Add function to move antelo background, translate)

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const setTheme= theme=>
 document.documentElement.className = theme;
 
-let ante = document.getElementById("antelope");
+//This function moves the background of the Neuro Section:
 window.addEventListener('scroll', function(){
-var value = window.scrollY;
-antelope.style.top =  value * 0.01 + 'px';
-console.log(antelope.style.top)
+let ante = document.getElementById("antelope")
+var value = window.scrollY * 0.01;
+ante.style.transform = 'translate(0px , '+value+'px)'
 })
+
+
+// // This function changes the scale of the background of the Neuro Section:
+// window.addEventListener('scroll', function(){
+//     let ante = document.getElementById("antelope")
+//     var value = window.scrollY * 0.0008;
+//     ante.style.transform = 'scale( '+value+')';
+//     console.log(value * 0.1)
+//     })
+
+
+// This function returns all css styles properties:
+// window.addEventListener('scroll', function(){
+//     const target = document.querySelector('.back');
+//     console.log(target.style)
+// })
